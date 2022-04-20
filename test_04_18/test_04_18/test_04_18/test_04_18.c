15eff9a7c788078fb6aab471f0b48dba1ae6c833
Ruie0024 <3158283275@qq.com>
2022-04-20 20:49:50 +0800

    the beginning of C program

---
+++ test_04_18/test_04_18/test_04_18/test_04_18.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//int main()
+//{
+//
+//	int a;
+//	scanf("%d", &a);
+//	printf("%x",a);
+//
+//	
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	char a;
+//	scanf("%d", &a);
+//	printf("%c", a);
+//	
+//	printf("\n%c",'\123');
+//	return 0;
+//}
+
+
+int main()
+{
+	printf("%s", "hello world!\n");
+
+	printf("hello world!\n");
+
+	puts("hello world!");
+	return 0;
+}
